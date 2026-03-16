15ac69c0f896e00754ce0b354b163aa86d3ba8bf
Vasja <vajsaman@gmail.com>
2026-03-16 20:52:01 +0100

    Improved dashboards: added next appointment, sorted appointments and edded price and payment status

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from appointments.models import Appointment
+from django.utils import timezone
 
 # Create your views here.
 
@@ -15,14 +16,29 @@ def home(request):
 
 @login_required
 def psychologist_dashboard(request):
-    appointments = Appointment.objects.filter(psychologist=request.user)
+    appointments = Appointment.objects.filter(psychologist=request.user).order_by('start_time')
+
+    next_appointment = appointments.filter(
+        start_time__gte=timezone.now()
+    ).order_by('start_time').first()
     return render(request, 'psychologist_dashboard.html', {
-        'appointments': appointments
+        'appointments': appointments,
+        'next_appointment': next_appointment
     })
 
 @login_required
 def client_dashboard(request):
-    appointments = Appointment.objects.filter(client=request.user)
+    appointments = Appointment.objects.filter(client=request.user).order_by('start_time')
+
+    next_appointment = appointments.filter(
+        start_time__gte=timezone.now()
+    ).order_by('start_time').first()
+
+    unpaid_appointments = appointments.filter(paid=False)
+    total_due = sum(a.price for a in unpaid_appointments)
+
     return render(request, 'client_dashboard.html', {
-        'appointments': appointments
+        'appointments': appointments,
+        'next_appointment': next_appointment,
+        'total_due': total_due
     })
