7e1dedeff2335073cc3c6fc6a178ffea29c19de1
Vasja <vajsaman@gmail.com>
2026-03-16 22:39:50 +0100

    Fix client detail page error

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from appointments.models import Appointment
 from django.utils import timezone
-from users.models import ClientProfile
+from users.models import ClientProfile, User
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
